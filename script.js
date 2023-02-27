9cb8e631d945d4e2a5cdb0dde3e8198d3c06458c
Matt <qpco@pm.me>
2023-02-27 12:44:41 -0500

    implement crit functionality. some layout changes.

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const defVal = document.getElementById('def-val')
 
 const atkDieList = document.getElementById('atk-die-list')
 const defDieList = document.getElementById('def-die-list')
+const atkCritList = document.getElementById('atk-crit-list')
+const defCritList = document.getElementById('def-crit-list')
 
 atkVal.innerHTML = atkSlider.value
 defVal.innerHTML = defSlider.value
@@ -29,10 +31,12 @@ function removeAllChildren(element) {
 
 function clearAllDice() {
     removeAllChildren(atkDieList)
+    removeAllChildren(atkCritList)
     removeAllChildren(defDieList)
+    removeAllChildren(defCritList)
 }
 
-async function assignImage(path) {
+function assignImage(path) {
     let button = document.createElement('button')
     let img = document.createElement('img')
     img.src = path
@@ -45,53 +49,79 @@ async function assignImage(path) {
 3 Side Swords - 50%
 1 Side Critical - 16.66%
 */
-async function attack() {
-    
-    removeAllChildren(atkDieList)
+let atkCrits = 0
+function atkRoll() {
     let button
-    for(let i = 0; i < atkSlider.value; i++) {
-        switch(Math.floor(Math.random()*6)+1) {
-            case 1:
-            case 2: 
-                button = await assignImage('./images/bow.png')
-                break
-            case 3: 
-            case 4:
-            case 5:
-                button = await assignImage('./images/sword.png')
-                break
-            case 6: 
-            button = await assignImage('./images/crit.png')
+    switch(Math.floor(Math.random()*6)+1) {
+        case 1:
+        case 2: 
+            button = assignImage('./images/bow.png')
             break
-        }
-        atkDieList.appendChild(button)
-        
+        case 3: 
+        case 4:
+        case 5:
+            button = assignImage('./images/sword.png')
+            break
+        case 6: 
+        atkCrits++
+        button = assignImage('./images/critAtk.png')
+        button.onclick = critAtk
+        break
     }
+    return button
 }
+
+function attack() { 
+    removeAllChildren(atkDieList)
+    removeAllChildren(atkCritList)
+    atkCrits = 0
+    for(let i = 0; i < atkSlider.value; i++) 
+        atkDieList.appendChild(atkRoll())
+
+}
+
+function critAtk() {
+    atkCrits--
+    if(atkCrits >= 0) 
+        atkCritList.appendChild(atkRoll())
+}
+
 /*
 4 Sides Blank - 66.66%
 1 Side Shield - 16.66%
 1 Side Critical - 16.66%
 */
-async function defend() {
-    removeAllChildren(defDieList)
+let defCrits = 0
+function defRoll() {
     let button
-    for(let i = 0; i < defSlider.value; i++) {
-        switch(Math.floor(Math.random()*6)+1) {
-            case 1:
-            case 2: 
-            case 3: 
-            case 4:
-                button = await assignImage('./images/blank.png')
-                break
-            case 5:
-                button = await assignImage('./images/shield.png')
-                break
-            case 6: 
-                button = await assignImage('./images/crit.png')
-                break
-        }
-        defDieList.appendChild(button)
-        
+    switch(Math.floor(Math.random()*6)+1) {
+        case 1:
+        case 2: 
+        case 3: 
+        case 4:
+            button = assignImage('./images/blank.png')
+            break
+        case 5:
+            button = assignImage('./images/shield.png')
+            break
+        case 6: 
+            defCrits++
+            button = assignImage('./images/crit.png')
+            button.onclick = critDef
+            break
     }
+    return button
+}
+
+function defend() {
+    removeAllChildren(defDieList)
+    removeAllChildren(defCritList)
+    for(let i = 0; i < defSlider.value; i++) 
+        defDieList.appendChild(defRoll())
+}
+
+function critDef() {
+    defCrits--
+    if(defCrits >= 0)
+        defCritList.appendChild(defRoll())
 }
